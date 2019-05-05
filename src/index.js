c11f743c376932f2471e1e64ba084c082a84c7dc
AvBorikDeveloper <avboris28@gmail.com>
2019-05-05 19:21:35 +1200

    completed the project file structure, except pages files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () =>{
+    return(
+        <div>
+            home
+        </div>
+        // <BrowserRouter>
+        //     <Routes/>
+        // </BrowserRouter>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
